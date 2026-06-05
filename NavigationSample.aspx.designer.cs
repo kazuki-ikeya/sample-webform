166819425511702cbaf3352ec32abaf0bcef6904
kazuki-ikeya <ikeya@org-net.co.jp>
2026-06-05 09:54:12 +0900

    メニューレイアウト

---
+++ NavigationSample.aspx.designer.cs
@@ -0,0 +1,22 @@
+namespace WebForm
+{
+    /// <summary>
+    /// NavigationSample.aspx の自動生成フィールド定義です。
+    /// </summary>
+    public partial class NavigationSample
+    {
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        protected global::System.Web.UI.WebControls.Label HeaderStatusLabel;
+
+        protected global::System.Web.UI.WebControls.TreeView SampleTreeView;
+
+        protected global::System.Web.UI.WebControls.Label ContentTitleLabel;
+
+        protected global::System.Web.UI.WebControls.Label ContentDescriptionLabel;
+
+        protected global::System.Web.UI.WebControls.Label SelectedValueLabel;
+
+        protected global::System.Web.UI.WebControls.Label SelectedPathLabel;
+    }
+}
